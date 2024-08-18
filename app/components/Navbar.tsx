88e3dbbea1e3e49e78a9d19d0c66fb21c594a43e
Jason Summers <jwsummers.dev@gmail.com>
2024-08-17 23:14:30 -0400

    add mobile responsiveness

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { FaBars, FaTimes } from 'react-icons/fa';
@@ -14,7 +16,7 @@ const Navbar: React.FC = () => {
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
         <div className="text-xl font-bold">
           <Link href="/">
-            My Portfolio
+            Jason Summers
           </Link>
         </div>
         <div className="md:hidden">
@@ -22,10 +24,10 @@ const Navbar: React.FC = () => {
             {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
           </button>
         </div>
-        <div className={`flex-col md:flex-row md:flex md:space-x-4 space-y-4 md:space-y-0 absolute md:relative w-full md:w-auto left-0 md:left-auto bg-gray-900 md:bg-transparent transition-all duration-300 ease-in-out ${isOpen ? 'top-12' : 'top-[-300px]'}`}>
-          <Link href="#about" className="hover:text-gray-300 px-4 py-2">About Me</Link>
-          <Link href="#projects" className="hover:text-gray-300 px-4 py-2">Projects</Link>
-          <Link href="#contact" className="hover:text-gray-300 px-4 py-2">Contact</Link>
+        <div className={`md:flex ${isOpen ? 'flex' : 'hidden'} flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0 absolute md:relative w-full md:w-auto left-0 md:left-auto bg-gray-900 md:bg-transparent transition-all duration-300 ease-in-out md:top-auto ${isOpen ? 'top-12' : 'top-[-300px] md:top-0'}`}>
+          <Link href="#about" className="hover:text-gray-300 px-4 py-2 md:px-0">About Me</Link>
+          <Link href="#projects" className="hover:text-gray-300 px-4 py-2 md:px-0">Projects</Link>
+          <Link href="#contact" className="hover:text-gray-300 px-4 py-2 md:px-0">Contact</Link>
         </div>
       </div>
     </nav>
