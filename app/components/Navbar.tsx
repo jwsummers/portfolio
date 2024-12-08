731e7c0457e75426cf815c76f53958c8dbb82a22
Jason Summers <jwsummers.dev@gmail.com>
2024-12-07 20:15:02 -0500

    Change Logo and add projects

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React, { useState } from 'react';
-import Link from 'next/link';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import React, { useState } from "react";
+import Link from "next/link";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Image from "next/image";
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,8 +20,13 @@ const Navbar: React.FC = () => {
     <nav className="fixed top-0 left-0 w-full bg-gray-900 text-white shadow-md z-50">
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
         <div className="text-xl font-bold">
-          <Link href="/" className='Name-Brand'>
-            Jason Summers
+          <Link href="/">
+            <Image
+              src="/images/JSlogo-rb.png"
+              alt="JS Logo"
+              width={65}
+              height={50}
+            />
           </Link>
         </div>
         <div className="md:hidden">
@@ -28,10 +34,22 @@ const Navbar: React.FC = () => {
             {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
           </button>
         </div>
-        <div className={`md:flex ${isOpen ? 'flex' : 'hidden'} flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0 absolute md:relative w-full md:w-auto left-0 md:left-auto bg-gray-900 md:bg-transparent transition-all duration-300 ease-in-out md:top-auto ${isOpen ? 'top-12' : 'top-[-300px] md:top-0'}`}>
-          <Link href="#about" className="nav-link" onClick={closeMenu}>About Me</Link>
-          <Link href="#projects" className="nav-link" onClick={closeMenu}>Projects</Link>
-          <Link href="#contact" className="nav-link" onClick={closeMenu}>Contact</Link>
+        <div
+          className={`md:flex ${
+            isOpen ? "flex" : "hidden"
+          } flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0 absolute md:relative w-full md:w-auto left-0 md:left-auto bg-gray-900 md:bg-transparent transition-all duration-300 ease-in-out md:top-auto ${
+            isOpen ? "top-12" : "top-[-300px] md:top-0"
+          }`}
+        >
+          <Link href="#about" className="nav-link" onClick={closeMenu}>
+            About Me
+          </Link>
+          <Link href="#projects" className="nav-link" onClick={closeMenu}>
+            Projects
+          </Link>
+          <Link href="#contact" className="nav-link" onClick={closeMenu}>
+            Contact
+          </Link>
         </div>
       </div>
     </nav>
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,67 +1,81 @@
-import React from 'react';
-import { Carousel } from 'primereact/carousel';
-import { Button } from 'primereact/button';
-import Image from 'next/image';
+import React from "react";
+import { Carousel } from "primereact/carousel";
+import { Button } from "primereact/button";
+import Image from "next/image";
 
 const projects = [
   {
-    title: 'TechTuned Web Design',
-    description: 'Website for TechTuned Web Design, a Web Development & Design agency',
-    image: '/images/TechTuned.png',
-    github: 'https://github.com/jwsummers/TechTunedWebDesign',
-    live: 'https://techtuned.netlify.app/',
+    title: "TechTuned Web Design",
+    description:
+      "Website for TechTuned Web Design, a Web Development & Design agency",
+    image: "/images/TechTuned.png",
+    github: "https://github.com/jwsummers/TechTunedWebDesign",
+    live: "https://techtunedwebdesign.com/",
   },
   {
-    title: 'OnTrack | Daily Food Logging',
-    description: 'A web app for tracking and logging daily food and water intake',
-    image: '/images/OnTrack.png',
-    github: 'https://github.com/jwsummers/OnTrack',
-    live: 'https://github.com/jwsummers/OnTrack',
+    title: "OnTrack | Daily Food Logging",
+    description:
+      "A web app for tracking and logging daily food and water intake",
+    image: "/images/OnTrack.png",
+    github: "https://github.com/jwsummers/OnTrack",
+    live: "https://github.com/jwsummers/OnTrack",
   },
   {
-    title: 'M.A.C.E | My Auto Care Expert',
-    description: 'A website for a small business to improve online presence, customer experience, and SEO',
-    image: '/images/mace.png',
-    github: 'https://github.com/jwsummers/mace',
-    live: 'https://myautocareexpert.com',
+    title: "7 Million Media LLC",
+    description:
+      "A website for a Professional Photographer to improve online presence, customer experience, and SEO",
+    image: "/images/7MM.png",
+    github: "https://github.com/jwsummers/7MillionMedia",
+    live: "https://7millionmedia.com",
   },
   {
-    title: 'Cook-E',
-    description: 'An online recipe book web app built with React, TypeScript, Postgres, and NodeJS',
-    image: '/images/cookE.png',
-    github: 'https://github.com/jwsummers/Cook-E',
-    live: 'https://github.com/jwsummers/Cook-E',
+    title: "M.A.C.E | My Auto Care Expert",
+    description:
+      "A website for a small business to improve online presence, customer experience, and SEO",
+    image: "/images/mace.png",
+    github: "https://github.com/jwsummers/mace",
+    live: "https://myautocareexpert.com",
   },
   {
-    title: 'T.R.E.D. | Tire Replacement Evaluation Device',
-    description: 'A web app for technicians to compare tire sizes to make decisions on replacement based off company standards',
-    image: '/images/tred.png',
-    github: 'https://github.com/jwsummers/Tire-Calculator',
-    live: 'https://tred.netlify.app/',
+    title: "Cook-E",
+    description:
+      "An online recipe book web app built with React, TypeScript, Postgres, and NodeJS",
+    image: "/images/cookE.png",
+    github: "https://github.com/jwsummers/Cook-E",
+    live: "https://github.com/jwsummers/Cook-E",
   },
   {
-    title: 'Labor Tracker',
-    description: 'A web app for tracking repair orders as well as commission labor hours',
-    image: '/images/laborTracker.png',
-    github: 'https://github.com/jwsummers/labor-tracker',
-    live: 'https://labortracker.netlify.app/',
+    title: "T.R.E.D. | Tire Replacement Evaluation Device",
+    description:
+      "A web app for technicians to compare tire sizes to make decisions on replacement based off company standards",
+    image: "/images/tred.png",
+    github: "https://github.com/jwsummers/Tire-Calculator",
+    live: "https://tred.netlify.app/",
+  },
+  {
+    title: "Labor Tracker",
+    description:
+      "A web app for tracking repair orders as well as commission labor hours",
+    image: "/images/laborTracker.png",
+    github: "https://github.com/jwsummers/labor-tracker",
+    live: "https://labortracker.netlify.app/",
   },
 ];
 
 const Projects: React.FC = () => {
   const responsiveOptions = [
     {
-      breakpoint: '1400px',
+      breakpoint: "1400px",
       numVisible: 2,
       numScroll: 1,
     },
     {
-      breakpoint: '1199px',
+      breakpoint: "1199px",
       numVisible: 2,
       numScroll: 1,
     },
     {
-      breakpoint: '767px',
+      breakpoint: "767px",
       numVisible: 1,
       numScroll: 1,
     },
@@ -87,13 +101,13 @@ const Projects: React.FC = () => {
               label="GitHub"
               icon="pi pi-github"
               className="p-button p-button-rounded"
-              onClick={() => window.open(project.github, '_blank')}
+              onClick={() => window.open(project.github, "_blank")}
             />
             <Button
               label="Live Demo"
               icon="pi pi-external-link"
               className="p-button-success p-button-rounded"
-              onClick={() => window.open(project.live, '_blank')}
+              onClick={() => window.open(project.live, "_blank")}
             />
           </div>
         </div>
@@ -104,7 +118,10 @@ const Projects: React.FC = () => {
   return (
     <div id="projects" className="bg-muted-gradient-2 py-12">
       <h2 className="section-heading">What I&apos;ve Done</h2>
-      <p className="mt-6 mb-4 p-4 text-center max-w-lg mx-auto">Here are some of my latest projects, feel free to inspect the code or live site by following the links provided.</p>
+      <p className="mt-6 mb-4 p-4 text-center max-w-lg mx-auto">
+        Here are some of my latest projects, feel free to inspect the code or
+        live site by following the links provided.
+      </p>
       <div className="max-w-6xl mx-auto">
         <Carousel
           value={projects}
