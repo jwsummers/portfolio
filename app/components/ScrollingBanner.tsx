e51bb266f128d77faff8705e89b78759fba56e8a
Jason Summers <jwsummers.dev@gmail.com>
2025-01-04 12:41:34 -0500

    Remove Next.js optimization from skill icons

--- app/components/ScrollingBanner.tsx
+++ app/components/ScrollingBanner.tsx
@@ -66,6 +66,7 @@ export default function ScrollingBanner({
               width={48}
               height={48}
               className='w-12 h-12'
+              unoptimized
             />
           </div>
         ))}
